1e875d831b6ddea100ca87e431923b44b2314f6b
Ivan Gundyrev <ivan.gundyrev@yandex.ru>
2024-10-15 02:29:58 +0300

    Исправлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import java.time.LocalDate;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FilmController.class)
 public class FilmControllerTest {
@@ -22,6 +27,9 @@ public class FilmControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private FilmService filmService;
 
     private Film film;
 
@@ -76,9 +84,36 @@ public class FilmControllerTest {
 
     @Test
     public void testValidFilm() throws Exception {
+        Film savedFilm = new Film();
+        savedFilm.setId(1);
+        savedFilm.setName(film.getName());
+        savedFilm.setDescription(film.getDescription());
+        savedFilm.setReleaseDate(film.getReleaseDate());
+        savedFilm.setDuration(film.getDuration());
+
+        when(filmService.addFilm(any(Film.class))).thenReturn(savedFilm);
+
         mockMvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(film)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(savedFilm.getId()))
+                .andExpect(jsonPath("$.name").value(savedFilm.getName()))
+                .andExpect(jsonPath("$.description").value(savedFilm.getDescription()))
+                .andExpect(jsonPath("$.releaseDate").value(savedFilm.getReleaseDate().toString()))
+                .andExpect(jsonPath("$.duration").value(savedFilm.getDuration()));
+    }
+
+    @Test
+    public void testServiceThrowsValidationException() throws Exception {
+        film.setName("");
+
+        when(filmService.addFilm(any(Film.class)))
+                .thenThrow(new ValidationException("Название фильма не может быть пустым"));
+
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(film)))
+                .andExpect(status().isBadRequest());
     }
 }
